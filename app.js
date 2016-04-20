44d6594d460bcd3344cbfbbfdd4a564341e1e3f8
josuetc94 <alu0100763492@ull.edu.es>
2016-04-20 17:03:36 +0000

    Instalando dependencias para mongo y configurando app.js para guardar registros en BD

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 
+//Conexión con MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Practica9');
+const TablaSchema = mongoose.Schema({
+    "entrada_actual": String
+});
+const Tabla = mongoose.model("Tabla",TablaSchema);
+    
 app.set('port', (process.env.PORT || 5000));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +29,25 @@ app.get('/', (request, response) => {
 });
 
 app.get('/csv', (request, response) => {
+    let t1 = new Tabla({entrada_actual: request.query.input});
+
+    let p1 = t1.save(function(err)
+    {
+        if(err)
+        {
+            console.log(`Hubieron errores:\n${err}`); return err; 
+        }
+        else
+        {
+            console.log(`Saved: ${t1}`);
+        }
+    });
+  
     response.send({ "rows": calculate(request.query.input) });
+});
+
+app.get('/mostrar_bd',(request, response) => { 
+    console.log("Mostrando el contenido de la bd"); 
 });
 
 app.listen(app.get('port'), () => {

cf0d90338b124e7af48768703ba33856c7cc8562
josuetc94 <alu0100763492@ull.edu.es>
2016-05-04 18:34:27 +0000

    Creando ruta buscar/usuario

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 
 //Conexión con Estructura de MongoDB
-const Tabla = require('./models/estructura_bd.js');
+//Conexión con Estructura de MongoDB
+const Estructura = require('./models/estructura_bd.js');
+const User = Estructura.User;
+const Tabla = Estructura.Tabla;
+console.log("Estructura:"+Estructura);
+console.log("User:"+User);
+console.log("Tabla:"+Tabla);
+const mongoose = require('mongoose');
+
 const util = require('util');
 
 app.set('port', (process.env.PORT || 5000));
@@ -44,6 +52,50 @@ app.get('/cargar_datos/:ejemplo',(request,response) => {
         response.send(data); //Servidor envia datos a csv.js
     });
 });
+
+app.param('usuario',function(req,res,next,usuario)
+{
+    if (usuario.match(/^[a-z0-9_]*$/i)) { 
+      req.ejemplo = usuario;
+    } else { 
+      next(new Error(`<${usuario}> does not match 'usuario' requirements`));
+    }
+    next();
+});
+
+app.get('/buscar/:usuario',(request,response) => {
+    console.log("Usuario a buscar:"+request.params.usuario);
+    User.find({nombre: request.params.usuario},function(err,data)
+    {
+        if(err)
+        {
+            console.error("Se ha producido un error->"+err);
+        }
+        else
+        {
+            console.log("Error:"+err);
+            console.log("Enviando datos a csv.js => Id de usuario:"+data[0]._id);
+            /*var id = new ObjectId(data[0]._id);
+            console.log("Id:"+id._id);*/
+            const id = mongoose.Types.ObjectId(data[0]._id);
+            console.log("Id:"+id);
+            Tabla.find({_creator: id},function(err,data_tablas)
+            {
+                if(err)
+                {
+                    console.error("Se ha producido un error->"+err);
+                }
+                else
+                {
+                    console.log("Enviando dattos a csv.js => Tablas asociadas:"+data_tablas);    
+                }
+                response.send({contenido: data_tablas, usuario_propietario: id});
+            });
+        }
+    });
+});
+
+
 
 app.get('/guardar_tabla/:ejemplo',(request, response) => { 
     console.log("Guardar tabla..."); 
